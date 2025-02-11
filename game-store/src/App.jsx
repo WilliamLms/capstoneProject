ffbd770bb2459df6ec2472ccc01d445eb16168ab
William Lemus <William10lms@gmail.com>
2025-02-11 00:21:00 -0500

    frontend login completed and also the links to go to different pages, will update later so it can work with the backend portion

--- game-store/src/App.jsx
+++ game-store/src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/Homepage";
 import Shoppingcart from "./pages/Shoppingcart";
+import Login from './pages/Login';
+// import Register from './pages/Register';
+import Header from './Links/Header';
 
 function App() {
   return (
@@ -11,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/cart" element={<Shoppingcart />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );

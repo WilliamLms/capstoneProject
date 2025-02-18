8e2fc35b0e04e2f828d0d31fe992ff649559cf28
Anthony Basco <abasco2@outlook.com>
2025-02-18 11:27:17 -0600

    Update Shoppingcart.jsx

--- game-store/src/pages/Shoppingcart.jsx
+++ game-store/src/pages/Shoppingcart.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useParams } from "react-router-dom";
 
 
+
 // products
 const products = [
   { id: 1, name: 'game 1 fantasy' , developer: 'placename', price: 40 },
@@ -9,6 +10,8 @@ const products = [
   { id: 3, name: 'game 3 flash rpg', developer: 'placename', price: 12 },
   { id: 4, name: 'game 4 free to play ', developer: 'placename', price: 0 },
 ];
+
+
 
 const ShoppingCart = () => {
   // State to manage the cart items
@@ -28,6 +31,17 @@ const ShoppingCart = () => {
   const calculateTotal = () => {
     return cart.reduce((total, item) => total + item.price, 0).toFixed(2);
   };
+
+  // checkout items in the cart
+ // const successful = () => {
+   // alert("Purchase successful")
+  //};
+
+  const Checkout = (productId) => {
+    setCart((prevCart) => prevCart.filter(item => item.id !== productId));
+    alert("GG! Your purchase was Successful, thank you for shopping at Game View! :)")
+  };
+  
 
   return (
     <div>
@@ -55,6 +69,23 @@ const ShoppingCart = () => {
               <li key={index}>
                 <span>{item.name} - ${item.price}</span>
                 <button onClick={() => removeItemFromCart(item.id)}>Remove</button>
+              </li>
+            ))
+          ) : (
+            <p>Your cart is empty</p>
+          )}
+        </ul>
+      </div>
+
+      {/* checkout */}
+      <div>
+        <h2>checkout</h2>
+        <ul>
+          {cart.length > 0 ? (
+            cart.map((item, index) => (
+              <li key={index}>
+                <span>{item.name} - ${item.price}</span>
+                <button onClick={() => Checkout(item.id)}>checkout</button>
               </li>
             ))
           ) : (

be33df5965906ddd51e56608493567a9c417ac28
Anthony Basco <abasco2@outlook.com>
2025-02-20 10:08:21 -0600

    Add files via upload

---
+++ Checkout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+//import './links/Header.css';
+
+
+
+const Checkout = () => {
+    return (
+        <div>
+            <h1>Your purchase was successful! Thank you for shopping at Game View!</h1>
+            <h2>Your order number is: 8675309</h2>
+        </div>
+        
+    )
+}
+
+export default Checkout;

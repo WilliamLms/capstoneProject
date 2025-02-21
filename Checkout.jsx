c612d233d5b59780febe953f12e3faa36d9959f0
Anthony Basco <abasco2@outlook.com>
2025-02-21 08:30:15 -0600

    Update Checkout.jsx

--- Checkout.jsx
+++ Checkout.jsx
@@ -8,6 +8,7 @@ const Checkout = () => {
         <div>
             <h1>Your purchase was successful! Thank you for shopping at Game View!</h1>
             <h2>Your order number is: 8675309</h2>
+            <h3>You can continue shopping by clicking on the shopping cart page!</h3>
         </div>
         
     )

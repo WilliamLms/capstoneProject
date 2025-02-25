4c9f60c721b20feb1f1fca64e2564bc45c994f8c
William Lemus <William10lms@gmail.com>
2025-02-25 01:24:41 -0500

    Backup before regenerating package-lock.json

--- game-store/src/pages/Login.jsx
+++ game-store/src/pages/Login.jsx
@@ -10,51 +10,60 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError("");
-  
+
     if (!email || !password) {
       setError("Please enter both Email and Password");
       return;
     }
-  
+
     try {
-      const response = await fetch('/api/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+      const response = await fetch("/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
       });
-  
+
       if (response.ok) {
         const data = await response.json();
         const { token } = data;
-        localStorage.setItem('authToken', token);
-        navigate('/');
+        localStorage.setItem("authToken", token);
+        navigate("/");
       } else {
-        setError('Invalid email or password.');
+        setError("Invalid email or password.");
       }
     } catch (error) {
-      console.error('Error during login:', error);
-      setError('An error occurred. Please try again later.');
+      console.error("Error during login:", error);
+      setError("An error occurred. Please try again later.");
     }
   };
-  
-return (
+
+  return (
     <div>
-        <h2>Login</h2>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-        <form onSubmit={handleSubmit}>
-            <div>
-               <label>Email:</label>
-               <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}
-               required/> 
-            </div>
-            <div>
-            <label>Password:</label>
-               <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}
-               required/> 
-            </div>
-            <button type="submit">Login</button>
-        </form>
+      <h2>Login</h2>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
-)};
+  );
+}
 
 export default Login;

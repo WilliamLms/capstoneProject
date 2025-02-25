e43bbb187fae6feccc20815fb96e83eb30d2f8f2
William Lemus <William10lms@gmail.com>
2025-02-25 10:29:52 -0500

    login fixed

--- game-store/src/pages/Login.jsx
+++ game-store/src/pages/Login.jsx
@@ -17,8 +17,8 @@ function Login() {
     }
 
     try {
-      const response = await fetch("/api/login", {
-        method: "POST",
+      const response = await fetch("http://localhost:5000/api/login", { 
+        method: "POST", 
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
       });

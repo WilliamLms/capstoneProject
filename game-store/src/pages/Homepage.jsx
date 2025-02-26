4bf1ba88017ca6599c5443f1e5942103e8f0e489
Anthony Basco <abasco2@outlook.com>
2025-02-26 12:35:37 -0600

    Update Homepage.jsx

--- game-store/src/pages/Homepage.jsx
+++ game-store/src/pages/Homepage.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 const Homepage = () => {
-    return (
-        <div>
-            <h1>Welcome to GameView</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Welcome to GameView</h1>
+      <h2>We have games like:</h2>
+      <img
+        src="https://i.redd.it/the-cover-art-for-elden-ring-contains-a-foreshadowing-to-v0-a4ji1zpv9ap81.jpg?width=1200&format=pjpg&auto=webp&s=89e59d9c3a33b0953e458b6dff0e5287172c7eb7"
+        ></img>
+      <img src ="https://upload.wikimedia.org/wikipedia/en/c/c6/The_Legend_of_Zelda_Breath_of_the_Wild.jpg"></img>
+      <img src = "https://upload.wikimedia.org/wikipedia/en/1/14/Halo_Infinite.png"></img>
+    </div>
+  );
+};
 
 export default Homepage;

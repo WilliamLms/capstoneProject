baf631ce9eded52195a975aa213c1ba43c201fa4
Anthony Basco <abasco2@outlook.com>
2025-02-27 14:21:30 -0600

    Update Homepage.jsx

--- game-store/src/pages/Homepage.jsx
+++ game-store/src/pages/Homepage.jsx
@@ -10,8 +10,14 @@ const Homepage = () => {
         ></img>
       <img src ="https://upload.wikimedia.org/wikipedia/en/c/c6/The_Legend_of_Zelda_Breath_of_the_Wild.jpg"></img>
       <img src = "https://upload.wikimedia.org/wikipedia/en/1/14/Halo_Infinite.png"></img>
+
+      <h3>BEST SELLERS TODAY</h3>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/en/4/4f/TLOU_P2_Box_Art_2.png"></img>
+      <img src ="https://upload.wikimedia.org/wikipedia/en/b/b6/Ghost_of_Tsushima.jpg"></img>
     </div>
   );
 };
 
 export default Homepage;
+

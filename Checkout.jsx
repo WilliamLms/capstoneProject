c89b8e443ff8702d96ab5674dc26d2bd7cc80fef
Anthony Basco <abasco2@outlook.com>
2025-02-27 08:45:53 -0600

    Update Checkout.jsx

--- Checkout.jsx
+++ Checkout.jsx
@@ -2,13 +2,14 @@ import React from "react";
 //import './links/Header.css';
 
 
+//this page can only be seen if you check out an item.
 
 const Checkout = () => {
     return (
         <div>
             <h1>Your purchase was successful! Thank you for shopping at Game View!</h1>
-            <h2>Your order number is: 8675309</h2>
-            <h3>You can continue shopping by clicking on the shopping cart page!</h3>
+            <h2>Your purchases will be waiting for you at your nearest GameView store. Your order code is: 8675309</h2>
+            <h3>Continue shopping by clicking on the shopping cart page.</h3>
         </div>
         
     )
